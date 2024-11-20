6304b09e2024006eac53888616cc73996b6a6a46
MunkhtTsolmon <monkhtsolmonmuzo@gmail.com>
2024-11-20 14:38:02 +0800

    hh

--- snake.js
+++ snake.js
@@ -1,6 +1,11 @@
-let headTop = 10;
-let headLeft = 12;
-let direction = "up";
+let headTop = 5;
+let headLeft = 5;
+let direction = "right";
+let tails = [
+  { x: 2, y: 5 },
+  { x: 3, y: 5 },
+  { x: 4, y: 5 },
+];
 
 const config = {
   size: 20,
@@ -42,6 +47,7 @@ function goLeft() {
   }
   render();
 }
+
 function changeDirection(newDirection) {
   if (direction === "up" || direction === "down") {
     if (newDirection === "right" || newDirection === "left") {
@@ -54,9 +60,30 @@ function changeDirection(newDirection) {
   }
 }
 
-setInterval(gameLoop, 300);
+let intervalID = null;
+
+function startGame() {
+  if (!intervalID) {
+    intervalID = setInterval(gameLoop, 300);
+  }
+}
+function reset() {
+  headTop = 5;
+  headLeft = 5;
+  direction = "right";
+}
+function restartGame() {
+  startGame();
+  reset();
+}
+function pauseGame() {
+  clearInterval(intervalID);
+  intervalID = null;
+}
 
 function gameLoop() {
+  tails.push({ x: headLeft, y: headTop });
+  tails.shift();
   switch (direction) {
     case "up":
       goUp();
@@ -73,14 +100,41 @@ function gameLoop() {
   }
 }
 
+function listenkeys(event) {
+  const key = event.key;
+  switch (key) {
+    case "ArrowUp":
+      changeDirection("up");
+      break;
+    case "ArrowDown":
+      changeDirection("down");
+      break;
+    case "ArrowLeft":
+      changeDirection("left");
+      break;
+    case "ArrowRight":
+      changeDirection("right");
+      break;
+  }
+}
+document.addEventListener("keydown", listenkeys);
+
 function render() {
-  const snakeHtml = `
+  let tailsHtml = ``;
+  for (let i = 0; i < tails.length; i++) {
+    tailsHtml += `<div class="snake" style="width: ${
+      1 * config.size
+    }px; height: ${1 * config.size}px; top: ${
+      tails[i].y * config.size
+    }px; left: ${tails[i].x * config.size}px"></div>`;
+  }
+  const headHtml = `
     <div class="snake" style="width: ${1 * config.size}px; height: ${
     1 * config.size
   }px; top: ${headTop * config.size}px; left: ${
     headLeft * config.size
   }px"></div>
     `;
+  const snakeHtml = `${headHtml} ${tailsHtml}`;
   boardEl.innerHTML = snakeHtml;
 }
-render();
